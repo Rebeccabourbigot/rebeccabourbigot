a326493053391aa4a1995dc892b0e6f744c4c4f3
Rébecca Bourbigot <rebeccabourbigot@icloud.com>
2020-02-10 16:35:22 +0100

    restart

---
+++ js/main0.js
@@ -0,0 +1,19 @@
+// // var openProject = function(a) {
+// //   $(a).find(".phone").css("margin-left","100px");
+// // }
+//
+// $(".content-01").on("click", function() {
+//   // var $this = $(this);
+//   // openProject($this);
+//   $(".grid-content").css("grid-template-columns","auto").css("grid-row-gap","50px");
+//   $(".grid-items").removeClass("right-shift");
+// });
+//
+// $(".content-02").on("click", function() {
+//   var $this = $(this);
+//   openProject($this);
+// });
+//var iWindowsSize = $(window).width();
+//if (iWindowsSize  < 950 && iWindowsSize >=550){
+//  console.log("cool");
+//  $(".grid-content").toggleClass("grid-content-mid");}
